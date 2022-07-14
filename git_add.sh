e84cdd1eb0012f88a02e2bcfa677303b3696d5b0
iyamnabeen <imnabeen01@gmail.com>
2022-07-14 16:50:04 +0545

    Comment : new icons

--- git_add.sh
+++ git_add.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
-git add .
-git commit -m "updated pkgs"
-git push origin
+#
+##################################################################################################################
+# Author 	: 	Nabeen Tiwaree
+# Website 	: 	https://iyammabeen.xyz
+##################################################################################################################
+
+git pull
+
+# Below command will backup everything inside the project folder
+git add --all .
+
+# Give a comment to the commit if you want
+echo "####################################"
+echo "Write your commit comment!"
+echo "####################################"
+
+input="new icons"
+git commit -m "Comment : $input "
+
+git push -u origin master
+
+
+echo "################################################################"
+echo "###################    Git Push Done      ######################"
+echo "################################################################"
