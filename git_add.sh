8313ae435f4e85edd705d8a3af85a729753a2ad8
iyamnabeen <imnabeen01@gmail.com>
2022-08-20 21:10:55 +0545

    update: automated script

--- git_add.sh
+++ git_add.sh
@@ -1,4 +1,27 @@
-#!/bin/bash
-git add .
-git commit -m "updated pkgs"
-git push origin
+#!/usr/bin/env bash
+
+init () {
+    git add --all .
+}
+
+commit_msg () {
+    echo "
+       /\_/\  +                             +
+  /\  / o o \ [ Enter your commit message ! ]
+ //\\\\ \~(*)~/ +                             +
+ \`  \/   ^ /
+    | \|| ||
+    \ '|| ||
+     \)()-())
+    "
+}
+
+final () {
+    read input
+    git commit -m "$input"
+    git push origin
+}
+
+init
+commit_msg
+final
