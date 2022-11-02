22d3737d496156a04a38da082ab90f05d5e16f24
iyamnabeen <imnabeen01@gmail.com>
2022-11-02 12:48:19 +0545

    add: Script to deal with metis repo

--- cleanup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cd x86_64/ && rm -rf metis.db* metis.files* && cd ../ && echo cleaned || echo error !
--- updaterepo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd x86_64
-repo-add -n -R metis.db.tar.gz *.pkg.tar.zst
-cd ../
